504ec93136703dc053525b2faa8737b508d6f248
Grant Gumina <grantgumina@outlook.com>
2015-04-30 11:49:31 -0400

    fixed bugz

--- main.rb
+++ main.rb
@@ -1,30 +1,35 @@
 require 'sinatra'
 
 
+set :port, 80
+
 get '/' do 
 
 	@data = []
 
-	File.open("test") do |file|
-		file.each_line do |line|
+	if File.file?("test") then
+		File.open("test") do |file|
+			file.each_line do |line|
 
-			x = line.split(",")[0]
-			y = line.split(",")[1]
-			z = line.split(",")[2]
-			num_objects = line.split(",")[3]
-			button_on = line.split(",")[4].strip!
+				x = line.split(",")[0]
+				y = line.split(",")[1]
+				z = line.split(",")[2]
+				num_objects = line.split(",")[3]
+				button_on = line.split(",")[4].strip!
 
-			array = [x, y, z, num_objects, button_on]
-			@data.push(array)
+				array = [x, y, z, num_objects, button_on]
+				@data.push(array)
+			end
 		end
 	end
 
-	@btn_on = @data[-1][4]
-
-	@total_objects = 0
-  	@data.each do |d| 
- 		@total_objects += d[3].to_i
- 	end
+	if !@data.empty? then
+		@btn_on = @data[-1][4]
+		@total_objects = @data[-1][3]
+	else
+		@btn_on = "0"
+		@total_objects = "n/a"
+	end
 
 	@index_active = "active"
 	@settings_active = ""
